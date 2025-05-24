ae0ab81622abc7a1fe5dab2ad8c0e21e75456b82
John Wang <johncwang@gmail.com>
2025-05-24 04:59:12 -0700

    feat: `rev5`: update `constants.go`

--- rev5/constants.go
+++ rev5/constants.go
@@ -1,5 +1,11 @@
 package rev5
 
 const (
+	ClassSP80053Enhancement = "SP800-53-enhancement"
+
 	GroupClassFamily = "family"
+
+	PropNameClass    = "class"
+	PropNameLabel    = "label"
+	PropClassSP80053 = "sp800-53a"
 )
