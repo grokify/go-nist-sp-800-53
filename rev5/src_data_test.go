b09750d2470975e65b96e190600d2c99a0137f87
John Wang <johncwang@gmail.com>
2025-05-25 10:34:31 -0700

    test: `rev5`: refactor tests

--- rev5/src_data_test.go
+++ rev5/src_data_test.go
@@ -2,9 +2,34 @@ package rev5
 
 import "testing"
 
-func TestControlSetHighUplift(t *testing.T) {
+func TestTierCatalogControls(t *testing.T) {
+	cat := CatalogAll()
 	ids := ControlIDs{}
-	testFn := func(t *testing.T, cs *ControlSet, tierName string) {
+	testFnCatalogControls := func(t *testing.T, cat *Catalog, label string, ctrIDs []string) {
+		t.Logf("testing existence of (%d) (%s) controls", len(ctrIDs), label)
+		for _, ctrID := range ctrIDs {
+			ctr, err := cat.Control(ctrID)
+			if err != nil {
+				t.Errorf("high uplift control not found(\"%s\") error: (%s)", ctrID, err.Error())
+			} else if ctr == nil {
+				t.Errorf("high uplift control found but is nil(\"%s\")", ctrID)
+			} else if ctr.ID != ctrID {
+				t.Errorf("high uplift control found but has mismatch(\"%s\") found (%s)", ctrID, ctr.ID)
+			}
+		}
+	}
+	for _, tierName := range Tiers() {
+		if tierIDs, err := ids.Tier(tierName); err != nil {
+			t.Errorf("tier not found(\"%s\") error: (%s)", tierName, err.Error())
+		} else {
+			testFnCatalogControls(t, cat, tierName, tierIDs)
+		}
+	}
+}
+
+func TestTierControlSets(t *testing.T) {
+	ids := ControlIDs{}
+	testFnControlSet := func(t *testing.T, cs *ControlSet, tierName string) {
 		if cs == nil {
 			t.Fatalf("no control set found for tier (%s)", tierName)
 		} else if cs.Len() == 0 {
@@ -28,9 +53,9 @@ func TestControlSetHighUplift(t *testing.T) {
 			}
 		}
 	}
-	testFn(t, ControlSetHighBaseline(), TierHighBaseline)
-	testFn(t, ControlSetHighUplift(), TierHighUplift)
-	testFn(t, ControlSetModerateBaseline(), TierModerateBaseline)
-	testFn(t, ControlSetModerateUplift(), TierModerateUplift)
-	testFn(t, ControlSetLowBaseline(), TierLowBaseline)
+	testFnControlSet(t, ControlSetHighBaseline(), TierHighBaseline)
+	testFnControlSet(t, ControlSetHighUplift(), TierHighUplift)
+	testFnControlSet(t, ControlSetModerateBaseline(), TierModerateBaseline)
+	testFnControlSet(t, ControlSetModerateUplift(), TierModerateUplift)
+	testFnControlSet(t, ControlSetLowBaseline(), TierLowBaseline)
 }
