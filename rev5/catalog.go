ffb1ed993f5b2d4670d452e72c3b3a71f7d73f80
John Wang <johncwang@gmail.com>
2025-05-24 22:03:55 -0700

    refactor: `rev5`: simplify code

--- rev5/catalog.go
+++ rev5/catalog.go
@@ -1,7 +1,6 @@
 package rev5
 
 import (
-	"fmt"
 	"sort"
 	"strings"
 
@@ -9,15 +8,6 @@ import (
 )
 
 type Catalog oscalTypes.Catalog
-
-type IDType string
-
-const (
-	IDTypeOSCAL     IDType = "id"
-	IDTypeOSCALSort IDType = "sort-id"
-	IDTypeNIST      IDType = "label"
-	IDTypeNISTSort  IDType = "sp800-53a"
-)
 
 func (c *Catalog) MustEnhancementIDs(idtype IDType, emptyOnError bool) []string {
 	if ids, err := c.EnhancementIDs(idtype); err == nil {
@@ -41,33 +31,11 @@ func (c *Catalog) EnhancementIDs(idtype IDType) ([]string, error) {
 					if controlw.Controls != nil {
 						for _, control := range *controlw.Controls {
 							if control.Class == ClassSP80053Enhancement {
-								switch idtype {
-								case IDTypeOSCAL:
-									out = append(out, control.ID)
-								default:
-									for _, prop := range *control.Props {
-										if idtype == IDTypeOSCALSort {
-											if prop.Name == string(idtype) {
-												if v := strings.TrimSpace(prop.Value); v != "" {
-													out = append(out, v)
-												}
-											}
-										} else if idtype == IDTypeNISTSort {
-											if prop.Name == PropNameLabel && prop.Class == string(idtype) {
-												if v := strings.TrimSpace(prop.Value); v != "" {
-													out = append(out, v)
-												}
-											}
-										} else if idtype == IDTypeNIST {
-											if prop.Name == PropNameLabel && prop.Class == "" {
-												if v := strings.TrimSpace(prop.Value); v != "" {
-													out = append(out, v)
-												}
-											}
-										} else {
-											return []string{}, fmt.Errorf("idtype not supported (%s)", idtype)
-										}
-									}
+								c2 := Control(control)
+								if idval, err := c2.IDForType(idtype); err != nil {
+									return []string{}, err
+								} else {
+									out = append(out, idval)
 								}
 							}
 						}
--- rev5/control_ids.go
+++ rev5/control_ids.go
@@ -1,18 +1,20 @@
 package rev5
 
-func ControlIDsHighBaseline() []string     { return Rev5ProfileHighBaseline().ControlIDs() }
-func ControlIDsModerateBaseline() []string { return Rev5ProfileModerateBaseline().ControlIDs() }
-func ControlIDsLowBaseline() []string      { return Rev5ProfileLowBaseline().ControlIDs() }
+type ControlIDs struct{}
 
-func ControlIDsHighUplift() []string {
-	return firstOnly(ControlIDsHighBaseline(), ControlIDsModerateBaseline())
+func (ids ControlIDs) HighBaseline() []string     { return Rev5ProfileHighBaseline().ControlIDs() }
+func (ids ControlIDs) ModerateBaseline() []string { return Rev5ProfileModerateBaseline().ControlIDs() }
+func (ids ControlIDs) LowBaseline() []string      { return Rev5ProfileLowBaseline().ControlIDs() }
+
+func (ids ControlIDs) HighUplift() []string {
+	return uplift(ids.HighBaseline(), ids.ModerateBaseline())
 }
 
-func ControlIDsModerateUplift() []string {
-	return firstOnly(ControlIDsModerateBaseline(), ControlIDsLowBaseline())
+func (ids ControlIDs) ModerateUplift() []string {
+	return uplift(ids.ModerateBaseline(), ids.LowBaseline())
 }
 
-func firstOnly(s1, s2 []string) []string {
+func uplift(s1, s2 []string) []string {
 	var out []string
 	prev := map[string]int{}
 	for _, s := range s2 {
@@ -26,13 +28,12 @@ func firstOnly(s1, s2 []string) []string {
 	return out
 }
 
-func ControlIDCounts() map[string]int {
+func (ids ControlIDs) Counts() map[string]int {
 	return map[string]int{
 		"all":               len(Rev5Catalog().MustEnhancementIDs(IDTypeOSCAL, false)),
-		"high_baseline":     len(ControlIDsHighBaseline()),
-		"high_uplift":       len(ControlIDsHighUplift()),
-		"moderate_baseline": len(ControlIDsModerateBaseline()),
-		"moderate_uplift":   len(ControlIDsModerateUplift()),
-		"low_baseline":      len(ControlIDsLowBaseline()),
-	}
+		"high_baseline":     len(ids.HighBaseline()),
+		"high_uplift":       len(ids.HighUplift()),
+		"moderate_baseline": len(ids.ModerateBaseline()),
+		"moderate_uplift":   len(ids.ModerateUplift()),
+		"low_baseline":      len(ids.LowBaseline())}
 }
