bd28f535ebb01488beb79fde7fefb9157373c546
John Wang <johncwang@gmail.com>
2025-05-27 04:53:49 -0700

    feat: `rev5`: add `SiteWriter{}` MkDocs support

---
+++ rev5/site_writer_mkdocs.go
@@ -0,0 +1,23 @@
+package rev5
+
+import mkdocs "github.com/grokify/go-mkdocs"
+
+func (sw SiteWriter) mkdocsConfigDefault() mkdocs.Config {
+	return mkdocs.Config{
+		SiteName: "NIST SP 800-53 Control Reference Site",
+		SiteURL:  "https://grokify.github.io/go-nist-sp-800-53",
+		DocsDir:  "docsrc",
+		Theme:    "readthedocs",
+		Nav: mkdocs.Texts{
+			{
+				Key: "Home",
+				Val: "index.md",
+			},
+		}}
+}
+
+func (sw SiteWriter) mkdocsWriteTOC(filename string, navTexts mkdocs.Texts) error {
+	cfg := sw.mkdocsConfigDefault()
+	cfg.Nav = append(cfg.Nav, navTexts...)
+	return cfg.WriteFileYAML(filename, 0600)
+}
