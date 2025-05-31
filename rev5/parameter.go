b83af8136f309518dbc4ba1857a1e24cdc6275cc
John Wang <johncwang@gmail.com>
2025-05-31 16:55:19 -0700

    feat: `rev5`: add `Parameter{}`, `ParameterSet{}`

---
+++ rev5/parameter.go
@@ -0,0 +1,85 @@
+package rev5
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	oscalTypes "github.com/defenseunicorns/go-oscal/src/types/oscal-1-1-3"
+	"github.com/grokify/mogo/fmt/fmtutil"
+)
+
+type Parameter oscalTypes.Parameter
+
+func (p Parameter) AltIDs() []string {
+	var out []string
+	if p.Props != nil {
+		for _, prop := range *p.Props {
+			if prop.Name == PropNameAltIdentifier {
+				if paramAltID := strings.TrimSpace(prop.Value); paramAltID != "" {
+					out = append(out, paramAltID)
+				}
+			}
+		}
+	}
+	return out
+}
+
+func (p Parameter) ProseLines() []string {
+	var out []string
+	if p.Guidelines != nil {
+		for _, gl := range *p.Guidelines {
+			out = append(out, gl.Prose)
+		}
+	}
+	return out
+}
+
+func (p Parameter) ProseString(sep string) string {
+	return strings.TrimSpace(strings.Join(p.ProseLines(), sep))
+}
+
+func (p Parameter) SelectString() string {
+	if p.Select == nil {
+		return ""
+	} else if p.Select.Choice == nil {
+		return ""
+	}
+	var conj string
+	switch p.Select.HowMany {
+	case "":
+		conj = "or"
+	case ParamHowManyOneOrMore:
+		conj = "and/or"
+	default:
+		fmtutil.PrintJSON(p)
+		panic(fmt.Sprintf("parameter.SelectString(): unknown how many (%s)", p.Select.HowMany))
+	}
+
+	parts := slices.Clone(*p.Select.Choice)
+	if len(parts) == 0 {
+		return ""
+	} else if len(parts) == 1 {
+		return strings.TrimSpace(parts[0])
+	} else if len(parts) == 2 {
+		return strings.TrimSpace(strings.Join(parts, conj))
+	} else {
+		if conj != "" {
+			parts[len(parts)-1] = conj + " " + parts[len(parts)-1]
+		}
+		return strings.TrimSpace(strings.Join(parts, ", "))
+	}
+}
+
+func (p Parameter) ValueString() string {
+	if v := p.ProseString(SepLines); v != "" {
+		return v
+	} else if v := p.SelectString(); v != "" {
+		return v
+	} else if v := strings.TrimSpace(p.Label); v != "" {
+		return v
+	} else {
+		fmtutil.PrintJSON(p)
+		panic("parameter display value string not found")
+	}
+}
---
+++ rev5/parameter_set.go
@@ -0,0 +1,129 @@
+package rev5
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+
+	oscalTypes "github.com/defenseunicorns/go-oscal/src/types/oscal-1-1-3"
+	"github.com/grokify/go-nist-sp-800-53/rev5/rev5tmpl"
+)
+
+type ParameterSet struct {
+	Data         map[string]Parameter
+	TemplateVars [][]string
+}
+
+func NewParameterSet() *ParameterSet {
+	return &ParameterSet{Data: map[string]Parameter{}}
+}
+
+func ParameterSetFromControl(ctr *Control) (*ParameterSet, error) {
+	if ctr == nil {
+		return nil, errors.New("control cannot be nil")
+	} else {
+		set := NewParameterSet()
+		return set, set.AddParameters(ctr.Params)
+	}
+}
+
+func (set *ParameterSet) AddParameters(params *[]oscalTypes.Parameter) error {
+	if params == nil {
+		return nil
+	}
+	for _, param := range *params {
+		if err := set.Add(Parameter(param)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (set *ParameterSet) Add(param Parameter) error {
+	if strings.TrimSpace(param.ID) == "" {
+		return errors.New("parameter id cannot be nil or empty")
+	}
+	if set.Data == nil {
+		set.Data = map[string]Parameter{}
+	}
+	if err := set.addByID(param.ID, param); err != nil {
+		return err
+	}
+	if altIDs := param.AltIDs(); len(altIDs) > 0 {
+		for _, altID := range altIDs {
+			if err := set.addByID(altID, param); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (set *ParameterSet) addByID(paramID string, param Parameter) error {
+	sep := "\n\n"
+	addProseString := param.ProseString(sep)
+	if try := set.Get(paramID); try != nil {
+		if existProseString := try.ProseString(sep); addProseString != existProseString {
+			return fmt.Errorf("prose mismatch, existing (%s) adding (%s)", existProseString, addProseString)
+		} else {
+			return nil
+		}
+	}
+	set.Data[paramID] = param
+	return nil
+}
+
+func (set *ParameterSet) Get(paramID string) *Parameter {
+	if set.Data == nil {
+		set.Data = map[string]Parameter{}
+	}
+	if param, ok := set.Data[paramID]; ok {
+		return &param
+	} else {
+		return nil
+	}
+}
+
+func (set *ParameterSet) Keys() []string {
+	var out []string
+	for k := range set.Data {
+		out = append(out, k)
+	}
+	sort.Strings(out)
+	return out
+}
+
+func (set *ParameterSet) MapKeyValue() map[string]string {
+	out := map[string]string{}
+	for k, param := range set.Data {
+		if v := param.ValueString(); v == "" {
+			panic("no value")
+		} else {
+			out[k] = v
+		}
+	}
+	return out
+}
+
+func (set *ParameterSet) ProseForParamID(paramID string) ([]string, error) {
+	var out []string
+	param, ok := set.Data[paramID]
+	if !ok {
+		return []string{}, errors.New("paramID not found")
+	}
+	if param.Guidelines != nil {
+		for _, gl := range *param.Guidelines {
+			if p := strings.TrimSpace(gl.Prose); p != "" {
+				out = append(out, p)
+			}
+		}
+	}
+	return out, nil
+}
+
+// RenderTemplate renders a template string with the paramters in the
+// paramter set.
+func (set *ParameterSet) RenderTemplate(template string) (string, error) {
+	return rev5tmpl.Render(template, set.MapKeyValue())
+}
