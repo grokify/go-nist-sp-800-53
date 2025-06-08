9765fa29af87f6fc70c75f7ddefdd313d40e9702
John Wang <johncwang@gmail.com>
2025-06-08 10:23:20 -0700

    feat: `rev5.IDSet{}`: add `NewIDSet()`, `IDSet.FilterBaseControls()`, `IDSet.IDs()`

--- rev5/id_set.go
+++ rev5/id_set.go
@@ -5,7 +5,11 @@ type IDSet struct {
 	Data map[string]ID
 }
 
-func (set *IDSet) AddID(ids ...ID) error {
+func NewIDSet() *IDSet {
+	return &IDSet{Data: map[string]ID{}}
+}
+
+func (set *IDSet) Add(ids ...ID) error {
 	for _, id := range ids {
 		if err := id.idPartsVerify(); err != nil {
 			return err
@@ -23,7 +27,7 @@ func (set *IDSet) AddIDString(s ...string) error {
 	for _, si := range s {
 		if id, err := ParseID(si); err != nil {
 			return err
-		} else if err := set.AddID(id); err != nil {
+		} else if err := set.Add(id); err != nil {
 			return err
 		}
 	}
@@ -34,6 +38,30 @@ func (set *IDSet) FamilyCounts() map[string]int {
 	out := map[string]int{}
 	for _, id := range set.Data {
 		out[id.FamilyAbbreviation]++
+	}
+	return out
+}
+
+func (set *IDSet) FilterBaseControls(inclEnhancementBase bool) (*IDSet, error) {
+	out := NewIDSet()
+	for _, id := range set.Data {
+		if id.EnhancementNumber <= 0 {
+			if err := out.Add(id); err != nil {
+				return nil, err
+			}
+		} else if idBase, err := id.IDBase(); err != nil {
+			return nil, err
+		} else if err := out.Add(idBase); err != nil {
+			return nil, err
+		}
+	}
+	return out, nil
+}
+
+func (set *IDSet) IDs() IDs {
+	var out []ID
+	for _, id := range set.Data {
+		out = append(out, id)
 	}
 	return out
 }
